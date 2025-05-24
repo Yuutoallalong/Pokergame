e713323ea705f15d96e555eab49a5ec2263edde1
Parathakorn Thanamee <6510615187@cn330>
2025-05-24 20:40:54 +0700

    implement Suit

--- src/main/java/com/pokerproject/common/Suit.java
+++ src/main/java/com/pokerproject/common/Suit.java
@@ -1,5 +1,24 @@
 package src.main.java.com.pokerproject.common;
 
-public class Suit {
+public enum Suit {
+    CLUBS("clubs"),
+    DIAMONDS("diamonds"),
+    HEARTS("hearts"),
+    SPADES("spades");
+
+    private final String symbol;
+
+    Suit(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
     
+    @Override
+    public String toString() {
+        return symbol;
+    }
+
 }
