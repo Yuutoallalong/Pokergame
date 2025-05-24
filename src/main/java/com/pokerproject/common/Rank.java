b294e3f8980c8708b9c35f2a9ea883b401aaf785
Parathakorn Thanamee <6510615187@cn330>
2025-05-24 20:34:18 +0700

    implement Rank

--- src/main/java/com/pokerproject/common/Rank.java
+++ src/main/java/com/pokerproject/common/Rank.java
@@ -1,5 +1,38 @@
 package src.main.java.com.pokerproject.common;
 
-public class Rank {
-    
+public enum Rank {
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13),
+    ACE(14);
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case ACE: return "A";
+            case KING: return "K";
+            case QUEEN: return "Q";
+            case JACK: return "J";
+            default: return String.valueOf(value);
+        }
+    }
 }
